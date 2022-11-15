927e686e3fe1773190b664346873ca233f6ad162
Johan Hansdes <johanhanses@gmail.com>
2022-11-15 09:02:02 +0100

    Add title to head

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (title: string) =>
+    title ? `${title} - Johan Hanses` : 'Johan Hanses'
+})
+</script>
+
 <template>
   <div
     class="bg-zinc-300 dark:bg-zinc-800 h-screen text-zinc-700 dark:text-zinc-300"
