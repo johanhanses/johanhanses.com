7d55342c733e8b3b8c6c9c3a54559e48a241cc74
Johan Hansdes <johanhanses@gmail.com>
2022-12-04 16:03:55 +0100

    fix color mode toggler bug

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,7 +5,7 @@ const colorMode = useColorMode()
 <template>
   <header class="w-full">
     <div class="flex space-x-6 h-full text-lg items-center">
-      <div class="mt-0.5">
+      <div class="mt-0.5 w-[32px]">
         <button
           v-if="colorMode.value === 'light'"
           aria-label="Set color mode to dark"
@@ -15,7 +15,7 @@ const colorMode = useColorMode()
           <Moon />
         </button>
         <button
-          v-else
+          v-if="colorMode.value === 'dark'"
           aria-label="Set color mode to light"
           class="hover:bg-zinc-700 px-4 py-1 rounded-md"
           @click="colorMode.preference = 'light'"
