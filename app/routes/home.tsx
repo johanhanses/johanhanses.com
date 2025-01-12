c0edd7936553199fc6ce10672364f959f1b75b3f
Johan Hanses <johanhanses@gmail.com>
2025-01-12 09:21:27 +0100

    test cd again

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,7 +7,7 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <main className="h-screen dark:bg-[#24273a] dark:text-[#cad3f5] text-[#4c4f69] bg-[#eff1f5] flex justify-center items-center">
-      <h1 className="text-xl">Hej</h1>
+      <h1 className="text-xl">Hejsan</h1>
     </main>
   )
 }
