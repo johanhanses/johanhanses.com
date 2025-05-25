2f7d0bc96d8f7bf1228e4d50184f74880f1dc239
Johan Hanses <johanhanses@gmail.com>
2025-05-25 17:38:29 +0200

    refactor: update styling and remove unused assets

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,3 @@
-import { Welcome } from '../welcome/welcome'
 import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
@@ -10,5 +9,31 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />
+  return (
+    <main className="flex items-center justify-center h-screen font-medium">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-serif">Johan Hanses</h1>
+
+        <p>
+          <a
+            href="https://https://www.linkedin.com/in/johanhanses"
+            target="_blank"
+            className="hover:text-[#528bff] hover:underline"
+          >
+            Linkedin
+          </a>
+        </p>
+
+        <p>
+          <a
+            href="https://https://github.com/johanhanses"
+            target="_blank"
+            className="hover:text-[#528bff] hover:underline"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
+    </main>
+  )
 }
